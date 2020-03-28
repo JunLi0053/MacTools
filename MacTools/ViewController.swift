2e963ff285b0c71eda75ebf629e27bf14a461e06
Jun <li514607336@163.com>
2020-03-28 15:54:47 +0800

    Initial Commit

---
+++ MacTools/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MacTools
+//
+//  Created by Jun on 2020/3/28.
+//  Copyright © 2020 Jun. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
